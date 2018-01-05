844caf371b961c62b0a7e239c91a87cd659cfea6
Charlie <n7best@gmail.com>
2018-01-05 19:26:06 +0000

    fix quick replies

--- app/codeReplies.js
+++ app/codeReplies.js
@@ -13,18 +13,18 @@ export default [
   },
   {
     key: "quickreply",
-    response: "<Message recipient={recipient}>        <Text>What's your favorite movie genre?</Text>        <QuickReply title=\"Search\" payload=\"<DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION>\"/>        <QuickReply title=\"Comedy\" payload=\"<DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY>\"/>        <QuickReply title=\"Drama\" payload=\"<DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA>\"/>      </Message>"
+    response: "class AwesomeChatBot extends Component {  render() {    const { recipient } = this.props;    return (      <Message recipient={recipient}>        <Text>What's your favorite movie genre?</Text>        <QuickReply title=\"Search\" payload=\"<DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION>\"/>        <QuickReply title=\"Comedy\" payload=\"<DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY>\"/>        <QuickReply title=\"Drama\" payload=\"<DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA>\"/>      </Message>    );  }}"
   },
   {
     key: "urlbutton",
-    response: "<Message recipient={recipient}>        <ButtonTemplate>          Display an URL Button          <URLButton url=\"https://www.oculus.com/en-us/rift/\">            Open Web URL          </URLButton>        </ButtonTemplate>      </Message>"
+    response: "class AwesomeChatBot extends Component {  render() {    const { recipient } = this.props;    return (      <Message recipient={recipient}>        <ButtonTemplate>          Display an URL Button          <URLButton url=\"https://www.oculus.com/en-us/rift/\">            Open Web URL          </URLButton>        </ButtonTemplate>      </Message>    );  }}"
   },
   {
     key: "postbackbutton",
-    response: "<Message recipient={recipient}>        <ButtonTemplate>          Display an Postback Button          <PostbackButton payload=\"DEVELOPER_DEFINED_PAYLOAD\">              Trigger Postback           </PostbackButton>        </ButtonTemplate>      </Message>"
+    response: "class AwesomeChatBot extends Component {  render() {    const { recipient } = this.props;    return (      <Message recipient={recipient}>        <ButtonTemplate>          Display an Postback Button          <PostbackButton payload=\"DEVELOPER_DEFINED_PAYLOAD\">              Trigger Postback           </PostbackButton>        </ButtonTemplate>      </Message>    );  }}"
   },
   {
     key: "sharebutton",
-    response: "<Message recipient={recipient}>        <ButtonTemplate>          Display an Share Button          <ShareButton>              Trigger Share           </ShareButton>        </ButtonTemplate>      </Message>"
+    response: "class AwesomeChatBot extends Component {  render() {    const { recipient } = this.props;    return (      <Message recipient={recipient}>        <ButtonTemplate>          Display an Share Button          <ShareButton>              Trigger Share           </ShareButton>        </ButtonTemplate>      </Message>    );  }}"
   },
 ]
