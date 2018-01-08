99dc5017498253258c68bfd4dbd57695c63ee6b1
Charlie <n7best@gmail.com>
2018-01-08 04:35:26 +0000

    update build files

---
+++ build/db.js
@@ -0,0 +1,113 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getRepliesByKey = exports.getRepliesById = exports.getReplies = exports.Reply = exports.sequelize = undefined;
+
+var _sequelize = require('sequelize');
+
+var _sequelize2 = _interopRequireDefault(_sequelize);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+var sequelize = exports.sequelize = new _sequelize2.default(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
+});
+
+var Reply = exports.Reply = sequelize.define('reply', {
+  id: {
+    type: _sequelize2.default.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  key: {
+    type: _sequelize2.default.STRING,
+    unique: true
+  },
+  response: {
+    type: _sequelize2.default.TEXT
+  }
+});
+
+var getReplies = exports.getReplies = function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return Reply.findAll();
+
+          case 2:
+            return _context.abrupt('return', _context.sent);
+
+          case 3:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined);
+  }));
+
+  return function getReplies() {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+var getRepliesById = exports.getRepliesById = function () {
+  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(id) {
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.next = 2;
+            return Reply.findOne({ where: { id: id } });
+
+          case 2:
+            return _context2.abrupt('return', _context2.sent);
+
+          case 3:
+          case 'end':
+            return _context2.stop();
+        }
+      }
+    }, _callee2, undefined);
+  }));
+
+  return function getRepliesById(_x) {
+    return _ref2.apply(this, arguments);
+  };
+}();
+
+var getRepliesByKey = exports.getRepliesByKey = function () {
+  var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(key, cb) {
+    var result;
+    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            _context3.next = 2;
+            return Reply.findOne({ where: { key: key } });
+
+          case 2:
+            result = _context3.sent;
+            return _context3.abrupt('return', result);
+
+          case 4:
+          case 'end':
+            return _context3.stop();
+        }
+      }
+    }, _callee3, undefined);
+  }));
+
+  return function getRepliesByKey(_x2, _x3) {
+    return _ref3.apply(this, arguments);
+  };
+}();
---
+++ build/seed.js
@@ -0,0 +1,57 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = [{
+  key: "helloworld",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><Text>Hello World!</Text></Message>); }}"
+}, {
+  key: "senderaction",
+  response: "const Typing = ({ recipient }) =>{ return (<SenderAction action={CONSTANTS.SENDER_ACTIONS.TYPING_ON} recipient={recipient} />) }"
+}, {
+  key: "attachment",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><Attachment url=\"https://react-messenger-ui.herokuapp.com/img/logo.png\" /></Message>); }}"
+}, {
+  key: "quickreply",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><Text>What's your favorite movie genre?</Text><QuickReply title=\"Search\" payload=\"<DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION>\"/><QuickReply title=\"Comedy\" payload=\"<DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY>\"/><QuickReply title=\"Drama\" payload=\"<DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA>\"/></Message>); }}"
+}, {
+  key: "urlbutton",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><ButtonTemplate>URL Button<URLButton url=\"https://www.oculus.com/en-us/rift/\">Open Web URL</URLButton></ButtonTemplate></Message>); }}"
+}, {
+  key: "postbackbutton",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><ButtonTemplate>Postback Button<PostbackButton payload=\"DEVELOPER_DEFINED_PAYLOAD\">Trigger Postback</PostbackButton></ButtonTemplate></Message>); }}"
+}, {
+  key: "sharebutton",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><GenericTemplate><GenericElement title=\"rift\" subtitle=\"next-generation virtual reality\" imageUrl=\"https://github.com/fbsamples/messenger-platform-samples/blob/master/node/public/assets/rift.png?raw=true\" ><ShareButton /></GenericElement></GenericTemplate></Message>); } }"
+}, {
+  key: "callbutton",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><ButtonTemplate>Call Button<CallButton payload=\"+16505551234\">Call Phone Number</CallButton></ButtonTemplate></Message>); } }"
+}, {
+  key: "loginbutton",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><ButtonTemplate>Login Button<LoginButton url=\"https://www.oculus.com/en-us/rift/\" /></ButtonTemplate></Message>); } }"
+}, {
+  key: "logoutbutton",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><ButtonTemplate>Logout Button<LogoutButton /></ButtonTemplate></Message>); } }"
+}, {
+  key: "gameplaybutton",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><ButtonTemplate>Game Button<GameplayButton payload=\"DEVELOPER_DEFINED_PAYLOAD\">Play Game</GameplayButton></ButtonTemplate></Message>); } }"
+}, {
+  key: "buttontemplate",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><ButtonTemplate>This is test text<URLButton url=\"https://www.oculus.com/en-us/rift/\">Open Web URL</URLButton><PostbackButton payload=\"DEVELOPER_DEFINED_PAYLOAD\">Trigger Postback</PostbackButton><CallButton payload=\"+16505551234\">Call Phone Number</CallButton></ButtonTemplate></Message>); } }"
+}, {
+  key: "generictemplate",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><GenericTemplate><GenericElement title=\"rift\" subtitle=\"next-generation virtual reality\" imageUrl=\"https://github.com/fbsamples/messenger-platform-samples/blob/master/node/public/assets/rift.png?raw=true\" ><URLButton url=\"https://www.oculus.com/en-us/rift/\">Open Web URL</URLButton><PostbackButton payload=\"magic payload\">Call Postback</PostbackButton></GenericElement></GenericTemplate></Message>); } }"
+}, {
+  key: "listtemplate",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><ListTemplate><ListElement title=\"rift\" subtitle=\"next-generation virtual reality\" imageUrl=\"https://github.com/fbsamples/messenger-platform-samples/blob/master/node/public/assets/rift.png?raw=true\" ><URLButton url=\"https://www.oculus.com/en-us/rift/\">Open Web URL</URLButton></ListElement><ListElement title=\"touch\" subtitle=\"Your Hands, Now in VR\" imageUrl=\"https://github.com/fbsamples/messenger-platform-samples/blob/master/node/public/assets/touch.png?raw=true\" ><URLButton url=\"https://www.oculus.com/en-us/touch/\">Open Web URL</URLButton></ListElement><URLButton url=\"https://www.oculus.com/en-us/touch/\">Open Web URL</URLButton></ListTemplate></Message>); } }"
+}, {
+  key: "mediatemplate",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><MediaTemplate><MediaElement type={CONSTANTS.MEDIA_TYPE.IMAGE} url=\"https://www.facebook.com/552483055096851/photos/a.552483101763513.1073741825.552483055096851/552484795096677/?type=3&theater\" /></MediaTemplate></Message>); } }"
+}, {
+  key: "opengraphtemplate",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><OpenGraphTemplate><OpenGraphElement url=\"https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb\"><URLButton url=\"https://en.wikipedia.org/wiki/Rickrolling\">View More</URLButton></OpenGraphElement></OpenGraphTemplate></Message>); } }"
+}, {
+  key: "receipttemplate",
+  response: "class AwesomeChatBot extends Component { render() { const { recipient } = this.props; return (<Message recipient={recipient}><ReceiptTemplate recipientName=\"Charlie\" orderNumber=\"12345678902\" paymentMethod=\"Visa 1234\" timestamp=\"1428444852\" ><Address street1=\"1 Hacker Way\" city=\"Menlo Park\" postalCode=\"94025\" state=\"CA\" country=\"USA\" /><Summary subtotal={75.00} shippingCost={4.95} totalTax={6.19} totalCost={56.14} /><Adjustment name=\"New Customer Discount\" amount={20} /><Adjustment name=\"$10 Off Coupon\" amount={10} /><ReceiptElement title=\"Classic White T-shirt\" subTitle=\"100% Soft and Luxurious Cotton\" quantity={2} price={50} currency=\"USD\" imageUrl=\"http://petersapparel.parseapp.com/img/whiteshirt.png\" /><ReceiptElement title=\"Classic Gray T-shirt\" subTitle=\"100% Soft and Luxurious Cotton\" quantity={1} price={25} currency=\"USD\" imageUrl=\"http://petersapparel.parseapp.com/img/grayshirt.png\" /></ReceiptTemplate></Message>); } }"
+}];
